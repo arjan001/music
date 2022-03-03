8f6ab1b811fe528375f2a0570645957edfd3164f
edwin nyongesa <arjannky@gmail.com>
2022-03-03 06:52:09 +0300

    trying out this music website code

---
+++ script.js
@@ -0,0 +1,13 @@
+var mysong =document.getElementById('mysong');
+var icon = document.getElementById('icon');
+
+icon.onclick = function() {
+   if(mysong.paused){
+       mysong.play();
+       icon.src="images/pause.png";
+   }
+   else{
+       mysong.pause();
+       icon.src="images/play.png";
+   }
+}
